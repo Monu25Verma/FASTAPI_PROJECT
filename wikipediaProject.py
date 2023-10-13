993bea35eeec6f8a366ac346b5eedea6966b4863
Monu25Verma <monuv2397@gmail.com>
2023-10-13 23:11:36 +0530

    retrivedata

--- wikipediaProject.py
+++ wikipediaProject.py
@@ -1,6 +1,6 @@
 from pydantic import Field, BaseModel
 import logging
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response, HTTPException
 #from dataset import Dataset
 import requests
 from pymongo import MongoClient
@@ -8,9 +8,13 @@ from dotenv import dotenv_values   #to call data from .env
 
 
 config  = dotenv_values(".env")
+
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)   #disable the loggers that are present when the function is called
+log = logging.getLogger(__name__)   # assign loggers with name
+
 #serilizer/ deserializer
 class wiki_data(BaseModel):
-    id : str
+    id : int
     title : str
     text : str
 
@@ -19,22 +23,45 @@ app = FastAPI()            # return in string format
 
 #to get title   
 @app.post("/{title}")                #url for title
-def get_title(request: Request, title : str):         #to create function for title
+def CreateDataset(request: Request, title : str):         #to create function for title
     wiki = get_wikipedia_page(title)   #class call to pass column name
     request.app.state.collection.insert_one(dict(wiki))
     return {'message':'Database Entry Done'}
 
 
-@app.get("/")
-def get_details()
+
+@app.get("/getData/{title}")
+def RetrieveDataset(request: Request, title : str):
+    response = request.app.state.collection.find_one({"title":title})
+    try:
+        message = wiki_data(id=response["id"],title=response["title"],text=response["text"])
+        return dict(message)
+    except TypeError:
+        raise HTTPException(
+            status_code = 404,
+            detail= f"{title} : Does not exist in DB",
+            
+        )
+    
+
+@app.get("/getall")
+def get_all_details(request:Request):
+    response = request.app.state.collection.find()
+    message = {}
+    n = 0
+    for i in response:
+        subMessage = wiki_data(id=i["id"],title=i["title"],text=i["text"])
+        message[n] = dict(subMessage)
+        n = n + 1
+    return message
 
 @app.on_event("startup")
 def startup_db_client():
     app.mongodb_client = MongoClient(config["MONGODB_CONNECTION_URI"])
     app.database = app.mongodb_client[config["DB_NAME"]]
-    collection = app.database.get_collection(config["COLLECTION_NAME"])
-    app.state.collection = collection
+    app.state.collection = app.database.get_collection(config["COLLECTION_NAME"])
     print("Connected to MONGODB database!")
+    log.info("Connected with Database")
 
 @app.on_event("shutdown")
 def shut_down_db_client():
@@ -60,7 +87,6 @@ def get_wikipedia_page(title):
         'title': page['title'],
         'text': page['extract'],
     }
-
 
 
 
